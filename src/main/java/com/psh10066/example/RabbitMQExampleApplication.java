4450ec8f081339ca17df9c0dea0741af9887cf7c
이호준 <psh10066@gmail.com>
2024-04-11 20:03:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/psh10066/example/RabbitMQExampleApplication.java
@@ -0,0 +1,13 @@
+package com.psh10066.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMQExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psh10066/example/RabbitMQExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psh10066.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMQExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
